3cdbe609ef19dd963c5eb2339862b72efc69849a
David Dada <daveproxy80@gmail.com>
2025-08-11 23:43:16 +0100

    chore: Ticket interface

---
+++ src/interfaces/ITicket.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.30;
+
+interface ITicket {
+    /// @notice Emitted when the base URI is updated
+    /// @param newBaseUri The new base URI set for the NFT collection
+    event BaseURIUpdated(string indexed newBaseUri);
+
+    /// @notice Emitted when the metadata of the NFT collection is updated
+    /// @param newName The new name of the NFT collection
+    event NameUpdated(string indexed newName);
+
+    /// @notice Emitted when the metadata of the NFT collection is updated
+    /// @param newSymbol The new symbol of the NFT collection
+    event SymbolUpdated(string indexed newSymbol);
+
+    /// @notice Updates the name of the NFT collection
+    function updateName(string calldata) external;
+
+    /// @notice Updates the symbol of the NFT collection
+    function updateSymbol(string calldata) external;
+
+    /// @notice Updates the base URI of the NFT collection
+    /// forge-lint: disable-next-line(mixed-case-function)
+    function updateURI(string calldata) external;
+
+    /// @notice Mints a new token to a given address
+    function mint(address) external returns (uint256);
+
+    /// @notice Pauses token transfers
+    function pause() external;
+
+    /// @notice Unpauses token transfers
+    function unpause() external;
+}

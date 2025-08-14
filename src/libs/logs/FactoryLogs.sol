5a0606aca05b6c1da16aaf0a011ef30b4550c0a9
David Dada <daveproxy80@gmail.com>
2025-08-14 22:44:07 +0100

    chore: add factory events/logs

---
+++ src/libs/logs/FactoryLogs.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.30;
+
+import {TicketMetadata} from "@host-it-storage/FactoryStorage.sol";
+
+event TicketCreated(uint256 indexed ticketId, address indexed ticketAdmin, TicketMetadata ticketData);
+
+event TicketUpdated(uint256 indexed ticketId, address indexed ticketAdmin, TicketMetadata ticketData);

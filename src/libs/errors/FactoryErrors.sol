ddb951d812fa1c614ec42155de57e11ede9d7bfc
David Dada <daveproxy80@gmail.com>
2025-08-14 22:43:26 +0100

    chore: add factory errors

---
+++ src/libs/errors/FactoryErrors.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.30;
+
+import {FeeType} from "@host-it-storage/MarketplaceStorage.sol";
+
+error EmptyName();
+error EmptyURI();
+error StartTimeShouldBeAhead();
+error EndTimeShouldBeOneDayAfterStartTime();
+error PurchaseStartTimeShouldBeOneDayBeforeStartTime();
+error MaxTicketsIsZero();
+error ArrayMismatch();
+error FeeAlreadySet(FeeType);
+error ZeroFee(FeeType);
+error TicketDoesNotExist(uint40);
+error TicketUseHasCommenced();
+error MaxTicketsShouldEqualSupply();

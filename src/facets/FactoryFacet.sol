0a8cf95ab62b49d9ffb9cf051f66f9a3949541aa
David Dada <daveproxy80@gmail.com>
2025-08-15 17:27:07 +0100

    chore: create factory facet

--- src/facets/FactoryFacet.sol
+++ src/facets/FactoryFacet.sol
@@ -1,1 +1,72 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.30;
 
+import {LibFactory} from "@host-it/libs/LibFactory.sol";
+import {TicketData, FullTicketData} from "@host-it-storage/FactoryStorage.sol";
+import {FeeType} from "@host-it-storage/MarketplaceStorage.sol";
+
+contract FactoryFacet {
+    using LibFactory for *;
+
+    //*//////////////////////////////////////////////////////////////////////////
+    //                             EXTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////////////////*//
+
+    function createTicket(TicketData calldata _ticketData, FeeType[] calldata _feeTypes, uint256[] calldata _fees)
+        external
+    {
+        _ticketData._createTicket(_feeTypes, _fees);
+    }
+
+    function updateTicket(TicketData calldata _ticketData, uint40 _ticketId) external {
+        _ticketData._updateTicket(_ticketId);
+    }
+
+    //*//////////////////////////////////////////////////////////////////////////
+    //                               VIEW FUNCTIONS
+    //////////////////////////////////////////////////////////////////////////*//
+
+    function ticketCount() public view returns (uint40) {
+        return LibFactory._getTicketCount();
+    }
+
+    function ticketExists(uint40 _ticketId) public view returns (bool) {
+        return _ticketId._ticketExists();
+    }
+
+    function ticketData(uint40 _ticketId) public view returns (FullTicketData memory) {
+        return _ticketId._getFullTicketData();
+    }
+
+    function allTicketData() public view returns (FullTicketData[] memory) {
+        return LibFactory._getAllFullTicketData();
+    }
+
+    function adminTickets(address _ticketAdmin) public view returns (uint40[] memory) {
+        return _ticketAdmin._getAdminTicketIds();
+    }
+
+    function adminTicketData(address _ticketAdmin) public view returns (FullTicketData[] memory) {
+        return _ticketAdmin._getAdminFullTicketData();
+    }
+
+    //*//////////////////////////////////////////////////////////////////////////
+    //                               PURE FUNCTIONS
+    //////////////////////////////////////////////////////////////////////////*//
+
+    function hostItTicketHash() public pure returns (bytes32) {
+        return LibFactory._getHostItTicketHash();
+    }
+
+    function ticketHash(uint40 _ticketId) public pure returns (bytes32) {
+        return _ticketId._generateTicketHash();
+    }
+
+    function mainAdminRole(uint40 _ticketId) public pure returns (uint256) {
+        return _ticketId._generateMainTicketAdminRole();
+    }
+
+    function ticketAdminRole(uint40 _ticketId) public pure returns (uint256) {
+        return _ticketId._generateTicketAdminRole();
+    }
+}

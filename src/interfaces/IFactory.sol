f4644a65818103a9f3fe13cb45b0da02a280558e
David Dada <daveproxy80@gmail.com>
2025-08-15 17:24:59 +0100

    chore: create factory interface

--- src/interfaces/IFactory.sol
+++ src/interfaces/IFactory.sol
@@ -1,1 +1,44 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.30;
 
+import {TicketData, FullTicketData} from "@host-it-storage/FactoryStorage.sol";
+import {FeeType} from "@host-it-storage/MarketplaceStorage.sol";
+
+interface IFactory {
+    //*//////////////////////////////////////////////////////////////////////////
+    //                             EXTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////////////////*//
+
+    function createTicket(TicketData calldata _ticketData, FeeType[] calldata _feeTypes, uint256[] calldata _fees)
+        external;
+
+    function updateTicket(TicketData calldata _ticketData, uint40 _ticketId) external;
+
+    //*//////////////////////////////////////////////////////////////////////////
+    //                               VIEW FUNCTIONS
+    //////////////////////////////////////////////////////////////////////////*//
+
+    function ticketCount() external view returns (uint40);
+
+    function ticketExists(uint40 _ticketId) external view returns (bool);
+
+    function ticketData(uint40 _ticketId) external view returns (FullTicketData memory);
+
+    function allTicketData() external view returns (FullTicketData[] memory);
+
+    function adminTickets(address _ticketAdmin) external view returns (uint40[] memory);
+
+    function adminTicketData(address _ticketAdmin) external view returns (FullTicketData[] memory);
+
+    //*//////////////////////////////////////////////////////////////////////////
+    //                               PURE FUNCTIONS
+    //////////////////////////////////////////////////////////////////////////*//
+
+    function hostItTicketHash() external pure returns (bytes32);
+
+    function ticketHash(uint40 _ticketId) external pure returns (bytes32);
+
+    function mainAdminRole(uint40 _ticketId) external pure returns (uint256);
+
+    function ticketAdminRole(uint40 _ticketId) external pure returns (uint256);
+}

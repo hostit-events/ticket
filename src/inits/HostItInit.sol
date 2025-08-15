1e249014c55fb90d49173153a75f106d1ab14c0b
David Dada <daveproxy80@gmail.com>
2025-08-15 17:45:26 +0100

    chore: start host-it init script

--- src/inits/HostItInit.sol
+++ src/inits/HostItInit.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.30;
 
+import {LibFactory} from "@host-it/libs/LibFactory.sol";
+
+event HostItInitialized();
+
+contract HostItInit {
+    function initHostIt(address _ticketImpl) external {
+        LibFactory._factoryStorage().ticketImplementation = _ticketImpl;
+
+        emit HostItInitialized();
+    }
+}

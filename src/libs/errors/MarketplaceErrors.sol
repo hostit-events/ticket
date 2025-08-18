4c72eae1e2e1b81908589b396a74fc120939e86a
David Dada <daveproxy80@gmail.com>
2025-08-18 19:57:38 +0100

    chore: add marketplace errors

---
+++ src/libs/errors/MarketplaceErrors.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.30;
+
+import {FeeType} from "@host-it-storage/MarketplaceStorage.sol";
+
+error PurchaseTimeNotReached();
+error TicketSoldOut();
+error MaxTicketsHeld();
+error TokenAddressZero();
+error InvalidFeeConfig();
+error FeeAlreadySet();
+error ZeroFee();
+error TicketIsFree();
+error FeeNotEnabled();
+error InsufficientBalance(address, FeeType, uint256);
+error InsufficientAllowance(address, FeeType, uint256);
+error WithdrawPeriodNotReached();
+error InsufficientWithdrawBalance();

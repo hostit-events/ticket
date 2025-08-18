9852445380e05b413e025467f2caec704a900426
David Dada <daveproxy80@gmail.com>
2025-08-18 19:57:24 +0100

    chore: add marketplace logs

---
+++ src/libs/logs/MarketplaceLogs.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.30;
+
+import {FeeType} from "@host-it-storage/MarketplaceStorage.sol";
+
+event TicketFeeSet(uint56 indexed ticketId, FeeType indexed feeType, uint256 fee);
+
+event TicketFeeAddressSet(FeeType indexed feeType, address indexed token);
+
+event TicketMinted(uint56 indexed ticketId, FeeType indexed feeType, uint256 fee, uint40 tokenId);

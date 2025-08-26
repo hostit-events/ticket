205ac7f6e2b0156c84820a448b252dc22476af6f
David Dada <daveproxy80@gmail.com>
2025-08-26 02:30:10 +0100

    chore: add more marketplace logs

--- src/libs/logs/MarketplaceLogs.sol
+++ src/libs/logs/MarketplaceLogs.sol
@@ -8,3 +8,7 @@ event TicketFeeSet(uint56 indexed ticketId, FeeType indexed feeType, uint256 fee
 event TicketFeeAddressSet(FeeType indexed feeType, address indexed token);
 
 event TicketMinted(uint56 indexed ticketId, FeeType indexed feeType, uint256 fee, uint40 tokenId);
+
+event TicketBalanceWithdrawn(uint56 indexed ticketId, FeeType indexed feeType, uint256 fee, address indexed to);
+
+event HostItBalanceWithdrawn(FeeType indexed feeType, uint256 fee, address indexed to);

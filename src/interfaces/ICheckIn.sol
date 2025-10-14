611e6d2898b75cb6137e61cbf03913ec4478dadd
David Dada <daveproxy80@gmail.com>
2025-10-14 12:33:54 +0100

    Add NatSpec comments

--- src/interfaces/ICheckIn.sol
+++ src/interfaces/ICheckIn.sol
@@ -1,26 +1,50 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.30;
 
+/// @title ICheckIn
+/// @notice Interface for checking in tickets
 interface ICheckIn {
     //*//////////////////////////////////////////////////////////////////////////
     //                             EXTERNAL FUNCTIONS
     //////////////////////////////////////////////////////////////////////////*//
 
+    /// @notice Checks in a ticket for a user
+    /// @param _ticketId The ID of the ticket to check in
+    /// @param _ticketOwner The owner of the ticket
+    /// @param _tokenId The token ID of the ticket
     function checkIn(uint64 _ticketId, address _ticketOwner, uint256 _tokenId) external;
 
+    /// @notice Adds ticket admins to a ticket
+    /// @param _ticketId The ID of the ticket to add admins to
+    /// @param _admins The addresses of the admins to add
     function addTicketAdmins(uint64 _ticketId, address[] calldata _admins) external;
 
+    /// @notice Removes ticket admins from a ticket
+    /// @param _ticketId The ID of the ticket to remove admins from
+    /// @param _admins The addresses of the admins to remove
     function removeTicketAdmins(uint64 _ticketId, address[] calldata _admins) external;
 
     //*//////////////////////////////////////////////////////////////////////////
     //                               VIEW FUNCTIONS
     //////////////////////////////////////////////////////////////////////////*//
 
+    /// @notice Checks if a ticket is checked in for a user
+    /// @param _ticketId The ID of the ticket to check
+    /// @param _ticketOwner The owner of the ticket
     function isCheckedIn(uint64 _ticketId, address _ticketOwner) external view returns (bool);
 
+    /// @notice Checks if a ticket is checked in for a user on a specific day
+    /// @param _ticketId The ID of the ticket to check
+    /// @param _day The day to check
+    /// @param _ticketOwner The owner of the ticket
     function isCheckedInForDay(uint64 _ticketId, uint8 _day, address _ticketOwner) external view returns (bool);
 
+    /// @notice Gets the list of users who have checked in for a ticket
+    /// @param _ticketId The ID of the ticket to get checked in users for
     function getCheckedIn(uint64 _ticketId) external view returns (address[] memory);
 
+    /// @notice Gets the list of users who have checked in for a ticket on a specific day
+    /// @param _ticketId The ID of the ticket to get checked in users for
+    /// @param _day The day to get checked in users for
     function getCheckedInForDay(uint64 _ticketId, uint8 _day) external view returns (address[] memory);
 }
--- src/interfaces/IFactory.sol
+++ src/interfaces/IFactory.sol
@@ -4,42 +4,78 @@ pragma solidity 0.8.30;
 import {FullTicketData, TicketData} from "@ticket-storage/FactoryStorage.sol";
 import {FeeType} from "@ticket-storage/MarketplaceStorage.sol";
 
+/// @title IFactory
+/// @notice Interface for the Factory facet
 interface IFactory {
     //*//////////////////////////////////////////////////////////////////////////
     //                             EXTERNAL FUNCTIONS
     //////////////////////////////////////////////////////////////////////////*//
 
+    /// @notice Creates a new ticket
+    /// @param _ticketData The ticket data
+    /// @param _feeTypes The fee types
+    /// @param _fees The fees
     function createTicket(TicketData calldata _ticketData, FeeType[] calldata _feeTypes, uint256[] calldata _fees)
         external
         returns (uint64);
 
+    /// @notice Updates an existing ticket
+    /// @param _ticketData The ticket data
+    /// @param _ticketId The ID of the ticket to update
     function updateTicket(TicketData calldata _ticketData, uint64 _ticketId) external;
 
     //*//////////////////////////////////////////////////////////////////////////
     //                               VIEW FUNCTIONS
     //////////////////////////////////////////////////////////////////////////*//
 
+    /// @notice Gets the total number of tickets
+    /// @return The total number of tickets
     function ticketCount() external view returns (uint64);
 
+    /// @notice Checks if a ticket exists
+    /// @param _ticketId The ID of the ticket to check
+    /// @return Whether the ticket exists
     function ticketExists(uint64 _ticketId) external view returns (bool);
 
+    /// @notice Gets the ticket data for a ticket
+    /// @param _ticketId The ID of the ticket to get data for
+    /// @return The ticket data
     function ticketData(uint64 _ticketId) external view returns (FullTicketData memory);
 
+    /// @notice Gets all ticket data
+    /// @return All ticket data
     function allTicketData() external view returns (FullTicketData[] memory);
 
+    /// @notice Gets the list of tickets for a ticket admin
+    /// @param _ticketAdmin The ticket admin to get tickets for
+    /// @return The list of tickets for the ticket admin
     function adminTickets(address _ticketAdmin) external view returns (uint64[] memory);
 
+    /// @notice Gets the ticket data for a ticket admin
+    /// @param _ticketAdmin The ticket admin to get ticket data for
+    /// @return The ticket data for the ticket admin
     function adminTicketData(address _ticketAdmin) external view returns (FullTicketData[] memory);
 
     //*//////////////////////////////////////////////////////////////////////////
     //                               PURE FUNCTIONS
     //////////////////////////////////////////////////////////////////////////*//
 
+    /// @notice Gets the hash of the HostIt ticket
+    /// @return The hash of the HostIt ticket
     function hostItTicketHash() external pure returns (bytes32);
 
+    /// @notice Gets the hash of a ticket
+    /// @param _ticketId The ID of the ticket to get the hash for
+    /// @return The hash of the ticket
     function ticketHash(uint64 _ticketId) external pure returns (bytes32);
 
+    /// @notice Gets the main admin role for a ticket
+    /// @param _ticketId The ID of the ticket to get the main admin role for
+    /// @return The main admin role for the ticket
     function mainAdminRole(uint64 _ticketId) external pure returns (uint256);
 
+    /// @notice Gets the ticket admin role for a ticket
+    /// @param _ticketId The ID of the ticket to get the ticket admin role for
+    /// @return The ticket admin role for the ticket
     function ticketAdminRole(uint64 _ticketId) external pure returns (uint256);
 }
